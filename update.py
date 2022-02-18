d32f06d33f0fefdb22535f8213aa6018398f5d30
ChrisDev8 <chrstr4251@outlook.com>
2022-02-18 17:23:46 -0500

    modified update.py

--- update.py
+++ update.py
@@ -1,4 +1,7 @@
 import subprocess
+import time
 
 while True:
-	subprocess.run(["git"], ["push"], ["-u"], ["origin"], ["main"])
+	subprocess.run(["git", "push", "-u", "origin", "main"])
+	time.sleep(9)
+	print("")
